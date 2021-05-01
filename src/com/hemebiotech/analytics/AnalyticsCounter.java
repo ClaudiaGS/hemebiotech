6a461dfcd42b1ae92c55e011aa9c17b551a66410
ClaudiaGugiuman <claudiagugiuman@yahoo.com>
2021-05-01 08:17:38 +0200

    Basic AnalyticsCounter with no read/write symptoms

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,43 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+	List<String> symptomsList = new ArrayList<String>();
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+	public AnalyticsCounter(List<String> symptomsList) {
+		this.symptomsList = symptomsList;
 	}
+
+	public Map AnalyticsCounter(List<String> symptomsList) {
+
+		Map<String, Integer> symptomsFrequency = new HashMap<String, Integer>();
+		int index = 0;
+		String currentSymptom = symptomsList.get(index);
+		int count = 0;
+
+		while (index < symptomsList.size() - 1) {
+			if (symptomsFrequency.containsKey(currentSymptom) == false) {
+				for (int i = 0; i < symptomsList.size(); i++) {
+					if (symptomsList.get(i) == currentSymptom) {
+						count += 1;
+						symptomsFrequency.put(currentSymptom, count);
+					}
+				}
+			}
+			count = 0;
+			index += 1;
+			currentSymptom = symptomsList.get(index);
+		}
+		return symptomsFrequency;
+
+
+	}
+
+
 }
+
